ae7e44bf98f362f75e7ba84f5c0a13fe4262635d
TH <thstudio6158@gmail.com>
2021-07-27 09:17:44 +0900

    FireBase V20210727_001

--- app/src/main/java/com/ths/thethskitchen_git_ver2021072601/MainActivity.kt
+++ app/src/main/java/com/ths/thethskitchen_git_ver2021072601/MainActivity.kt
@@ -4,11 +4,16 @@ import com.ths.thethskitchen_git_ver2021072601.databinding.ActivityMainBinding
 
 import android.os.Bundle
 import android.os.StrictMode
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-//import com.google.firebase.FirebaseApp
-//import com.google.firebase.appcheck.FirebaseAppCheck
-//import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 import com.ths.a20210713_sqllitehelper.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -27,6 +32,7 @@ suspend fun downloadList(inputStream: InputStream?, range: String, )
 class MainActivity : AppCompatActivity() {
     val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
     val helper = SqliteHelper(this,"dlist",1)
+    private lateinit var database : DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,9 +57,7 @@ class MainActivity : AppCompatActivity() {
             adapter.listData.addAll(helper.selectDlist())
         }
     }
-
-
-
+    
 
 }
 

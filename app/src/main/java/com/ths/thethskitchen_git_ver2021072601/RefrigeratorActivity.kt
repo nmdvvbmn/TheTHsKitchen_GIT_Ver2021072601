17c68d6323a678c885cde2cb53e3b42553538d20
TH <thstuido6158@gmail.com>
2021-07-30 11:40:56 +0900

    FireBase20210729_drawer_menu

---
+++ app/src/main/java/com/ths/thethskitchen_git_ver2021072601/RefrigeratorActivity.kt
@@ -0,0 +1,11 @@
+package com.ths.thethskitchen_git_ver2021072601
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RefrigeratorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_refrigerator)
+    }
+}

90260eb6f28c59cb562cd0f1937df814a15f6a92
TH <thstuido6158@gmail.com>
2021-07-30 21:59:57 +0900

    FireBase20210730_kitchen

--- app/src/main/java/com/ths/thethskitchen_git_ver2021072601/UtilFuncs.kt
+++ app/src/main/java/com/ths/thethskitchen_git_ver2021072601/UtilFuncs.kt
@@ -7,14 +7,9 @@ open class UtilFuncs {
     open fun getLanguage(): String{
         var language = Locale.getDefault().getLanguage()
         if (language != null) {
-            val codeList: List<String> = listOf("ko","en","zh-TW","zh-HK","gu","el","nl","ne","no",
-                "da","de","lo","lv","la","ru","ro","lb","lt","mr","mk","ms","mn","eu","my","vi",
-                "be","bn","bs","bg","sr","su","si","sw","sv","gd","es","es-419","es-es","sk","sl",
-                "sd","ar","hy","is","ga","az","af","sq","am","eo","et","en-us","en-gb","or","ur",
-                "uz","uk","cy","it","id","ja","jw","ka","zh-CN","cs","kk","ca","hr","km","ky","ta",
-                "th","tr","ps","pa","fa","pt","pt-br","pt-pt","pl","hu","fr","fr-fr","fi","tl","he",
-                "hi","GL","st","rw","mg","ml","yo","ig","yi","zu","kn","xh","ku","tg","tt","te",
-                "tk","ha")
+            val codeList: List<String> = listOf("ko", "en", "zh-CN", "zh-TW", "es", "ar", "hi",
+                "bn", "pt",	"ru", "ja",	"jw", "tr", "fr", "de", "te", "mr", "ur", "vi", "ta", "it",
+                "fa", "ms", "gu")
             if (codeList.any { it == language } ) {
                 return language
             }else{

4e923c18b17d530c7ba7f52bb6d300a67763f579
TH <thstuido6158@gmail.com>
2021-08-12 20:18:23 +0900

    20210812_Function complete

--- app/src/main/java/com/ths/thethskitchen_git_ver2021072601/IntroActivity.kt
+++ app/src/main/java/com/ths/thethskitchen_git_ver2021072601/IntroActivity.kt
@@ -1,11 +1,14 @@
 package com.ths.thethskitchen_git_ver2021072601
 
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
 import android.util.Log
+import android.view.animation.AnimationUtils
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.whenResumed
 import com.ths.thethskitchen_git_ver2021072601.databinding.ActivityIntroBinding
@@ -17,10 +20,16 @@ class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.imgIntro.startAnimation(AnimationUtils.loadAnimation(applicationContext,R.anim.rotate))
 
         lifecycleScope.launch {
             whenResumed {
-                    delay(3000)
+                    delay(1500)
+                    val colorChange = AnimatorInflater.loadAnimator(applicationContext,R.anim.color) as AnimatorSet
+                    colorChange.setTarget(binding.viewIntro)
+                    colorChange.start()
+                    delay(1500)
+                    colorChange.end()
                     val intent = Intent(baseContext, MainActivity::class.java)
                     startActivity(intent)
                     finish()
